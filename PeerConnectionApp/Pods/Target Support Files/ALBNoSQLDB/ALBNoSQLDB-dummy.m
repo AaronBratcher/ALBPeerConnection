25b9fd61df8be10110322fe8f000771d69cd62a6
Aaron Bratcher <aaronbratcher@Aarons-MacBook-Pro-2.local>
2017-02-01 09:05:49 -0500

    Remove pod files

--- PeerConnectionApp/Pods/Target Support Files/ALBNoSQLDB/ALBNoSQLDB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ALBNoSQLDB : NSObject
-@end
-@implementation PodsDummy_ALBNoSQLDB
-@end
--- PeerConnectionApp/Pods/Target Support Files/ALBPeerConnection/ALBPeerConnection-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ALBPeerConnection : NSObject
-@end
-@implementation PodsDummy_ALBPeerConnection
-@end
